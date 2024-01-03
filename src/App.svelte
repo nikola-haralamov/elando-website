c12d274389cd72b3ca87372ef8f034f4f8fcb1c6
Nikola Haralamov <nikola.haralamov@elando.com>
2024-01-03 11:21:58 +0200

    menu and others

--- src/App.svelte
+++ src/App.svelte
@@ -5,20 +5,77 @@
 </script>
 
 <header>
-  <div class="container">
-    <img src="./logo.svg" alt="logo" />
-    <nav>
-      <a href="/" on:click|preventDefault={() => (menu = 1)}>Home</a>
-      <a href="/about" on:click|preventDefault={() => (menu = 2)}>About</a>
+  <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+    <div class="flex items-center flex-shrink-0 text-white mr-6">
+      <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+      <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+    </div>
+    <div class="block lg:hidden">
+      <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      </button>
+    </div>
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="text-sm lg:flex-grow">
+        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+          Docs
+        </a>
+        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+          Examples
+        </a>
+        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+          Blog
+        </a>
+      </div>z
+      <div>
+        <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+      </div>
+    </div>
+  </nav>
+
+
+
+  <div class="flex justify-row flex-wrap justify-center items-center text-center">
+
+    <nav class="flex items-center justify-between flex-wrap p-6">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <img src="./logo.svg" alt="logo" />
+      </div>
+      <div class="block lg:hidden">
+        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
+          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Docs
+          </a>
+          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Examples
+          </a>
+          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+            Blog
+          </a>
+        </div>z
+        <div>
+          <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+        </div>
+      </div>
     </nav>
   </div>
 </header>
 
+
+<div>
+  <img src="./logo.svg" alt="logo" />
+</div>
+<nav>
+  <a href="/" on:click|preventDefault={() => (menu = 1)}>Home</a>
+  <a href="/about" on:click|preventDefault={() => (menu = 2)}>About</a>
+</nav>
+
 <main>
-
-  <div class="container">
-  </div>
-
 
   {#if menu === 1}
     <Home />
@@ -29,32 +86,11 @@
       Page Not Found
     </h1>
   {/if}
-
-  <div class="leftaa"></div>
 </main>
 
 <style>
   ul#menu li{
     display : inline;
-  }
-
-  .leftaa {
-    z-index: -1;
-    top: 10rem;
-    left: -30rem;
-    position: absolute;
-    fill: var(--Cyan, #00E5FF);
-    mix-blend-mode: screen;
-    filter: blur(230px);
-    width: 838.17px;
-    height: 838.17px;
-    transform: rotate(-60deg);
-    transform-origin: 0 0;
-    opacity: 0.25;
-    background: #00E5FF;
-    /*box-shadow: 460px 460px 460px; */
-    /*filter: blur(460px)*/
-
   }
 
 </style>
--- src/Pages/Home.svelte
+++ src/Pages/Home.svelte
@@ -1,3 +1,14 @@
-<h1>
-  Home
-</h1>
+<section class="section relative overflow-hidden bg-[#c4c4c4] bg-opacity-[0.02]">
+  <div class="glow-brass blur-large -top-72 -left-72 w-[560px] h-[560px] -z-10" aria-hidden="true"></div>
+  <div class="glow-brass blur-large top-1/2 -right-32 w-[560px] h-[560px] -z-10" aria-hidden="true" style="animation-delay: 4s"></div>
+
+
+  <div class="flex justify-between">
+    <div>
+      <p>text</p>
+    </div>
+    <div>
+      <img src="./home-top-left.svg" alt="home-top-left" />
+    </div>
+  </div>
+</section>
