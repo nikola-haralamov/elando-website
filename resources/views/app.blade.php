1aa9a5423fb1574fd36214381a1785ef5450db27
Nikola Haralamov <nikola.haralamov@elando.com>
2024-01-03 14:57:19 +0200

    layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,10 +1,13 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/js/app.js')
     @inertiaHead
+    <title>Document</title>
 </head>
 <body>
 @inertia
